ad042b4b368cc953c0a8d2d71476f2278e05d194
Sergio Brito <sergiobritor@gmail.com>
2020-11-16 02:41:45 -0500

    feat(publish libray) reset unused files

--- app/src/interfaces/Validation.ts
+++
@@ -1,8 +0,0 @@
-export interface StringValidator {
-  isAcceptable(s: string): boolean;
-}
-
-/** Interfaz para todos los usuarios */
-export interface CustomUser {
-  name: string;
-}
--- app/src/validators/LettersOnlyValidator.ts
+++
@@ -1,9 +0,0 @@
-import { StringValidator } from '../interfaces/Validation';
-
-const lettersRegexp = /^[A-Za-z]+$/;
-
-export class LettersOnlyValidator implements StringValidator {
-  isAcceptable(s: string) {
-    return lettersRegexp.test(s);
-  }
-}
