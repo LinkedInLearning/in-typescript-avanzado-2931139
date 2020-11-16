6816aceed24cf41d1e3ce9dbc449c8f9136b76d1
Sergio Brito <sergiobritor@gmail.com>
2020-11-15 21:02:50 -0500

    feat(modules) use a barrel file

--- library/src/content.ts
+++ library/src/content.ts
@@ -1,6 +1,8 @@
-import { ComplicatedJoke as HardJoke, Joke } from './interfaces/joke.interface';
-import { Quote } from './interfaces/quote.interface';
-import { Riddle } from './interfaces/riddle.interface';
+// import { ComplicatedJoke as HardJoke, Joke } from './interfaces/joke.interface';
+// import { Quote } from './interfaces/quote.interface';
+// import { Riddle } from './interfaces/riddle.interface';
+
+import { ComplicatedJoke as HardJoke, Joke, Quote, Riddle } from './interfaces';
 
 export const quote: Quote = {
   quote:
---
+++ library/src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export * from './common.types';
+export * from './joke.interface';
+export * from './quote.interface';
+export * from './riddle.interface';
