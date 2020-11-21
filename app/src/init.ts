d63375b7becde1ba1ae07632395d39ab7501c49d
Sergio Brito <sergiobritor@gmail.com>
2020-11-20 20:50:35 -0500

    feat(publish libray) install and initialize jQuery

--- app/src/init.ts
+++ app/src/init.ts
@@ -1,4 +1,7 @@
+import $ from 'jquery';
 import { makeA } from 'yfx-in-modules';
 export { makeA as picker };
 
-console.log('Hola Mundo');
+$(() => {
+  console.log('Documento listo para usar jQuery');
+});
