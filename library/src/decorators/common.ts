1cff3e87a3e8925faa9e8f9ec6ed46e809c6ebf8
Sergio Brito <sergiobritor@gmail.com>
2020-11-22 00:49:16 -0500

    feat(decorators) comment unused console.logs

--- library/src/decorators/common.ts
+++ library/src/decorators/common.ts
@@ -1,5 +1,5 @@
 export function Logger(message: string) {
-  //   console.log(`Mensaje: ${message}`);
+  //  console.log(`Mensaje: ${message}`);
 
   return function (target: any) {
     // console.log(`Clase decorada: ${target}`);
@@ -8,34 +8,34 @@ export function Logger(message: string) {
 }
 
 export function connect() {
-  console.log('Decorador CONNECT --> evaluado');
+  // console.log('Decorador CONNECT --> evaluado');
   return function (
     target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
-    console.log('Decorador CONNECT  --> invocado');
+    // console.log('Decorador CONNECT  --> invocado');
   };
 }
 
 export function counter() {
-  console.log('Decorador COUNTER --> evaluado');
+  // console.log('Decorador COUNTER --> evaluado');
   return function (
     target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
-    console.log('Decorador COUNTER  --> invocado');
+    // console.log('Decorador COUNTER  --> invocado');
   };
 }
 
 export function test() {
-  console.log('Decorador TEST --> evaluado');
+  // console.log('Decorador TEST --> evaluado');
   return function (
     target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
-    console.log('Decorador TEST  --> invocado');
+    // console.log('Decorador TEST  --> invocado');
   };
 }
