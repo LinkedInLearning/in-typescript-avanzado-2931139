665d8af54c3a901829c1abfa90c5b3034ad7e0b3
Sergio Brito <sergiobritor@gmail.com>
2020-11-22 23:56:02 -0500

    feat(advanced datatypes) use index types

--- library/src/interfaces/quote.interface.ts
+++ library/src/interfaces/quote.interface.ts
@@ -2,4 +2,29 @@
 export interface Quote {
   quote: string;
   author: string;
+  [key: string]: string;
 }
+
+type QuoteKey = keyof Quote;
+
+type quoteType = Quote['quote'];
+type authorType = Quote['author'];
+
+function getPropertyValue<T, U extends keyof T>(obj: T, propertyName: U): T[U] {
+  return obj[propertyName];
+}
+
+const quote: Quote = {
+  author: 'yacafx',
+  quote: 'Happy Coding',
+  date: new Date().toString(),
+};
+
+const quoteContent = getPropertyValue(quote, 'quote');
+const authorContent = getPropertyValue(quote, 'author');
+
+function getProperties<T, U extends keyof T>(obj: T, props: U[]): T[U][] {
+  return props.map((name) => obj[name]);
+}
+
+getProperties(quote, ['quote', 'author', 'date']);
