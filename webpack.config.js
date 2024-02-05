b0c34230ea4d964d37905b0bf32377a1413dfc81
edwardv1 <edwardismael1@gmail.com>
2024-02-05 11:21:13 -0300

    package.json, ts and webpack config created

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const config = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
+};
